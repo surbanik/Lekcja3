afc5a951a204b3808ee0259d77e75ed63720dca9
surbanik <surbanik@sii.pl>
2021-11-15 09:19:00 +0100

    All not used setters and getters have been removed.

--- src/test/java/Employee.java
+++ src/test/java/Employee.java
@@ -10,28 +10,8 @@ public class Employee {
         this.salary = salary;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public int getSalary() {
         return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
     }
 
     public String getAllData (){
